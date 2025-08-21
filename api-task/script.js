fcedd5073d0f4b812f8b91bf677a9d190b526494
Piyush Sachdeva <PiyushSchdva>
2025-08-21 13:28:21 +0530

    Api Task

---
+++ api-task/script.js
@@ -0,0 +1,25 @@
+
+  const postsContainer = document.getElementById('posts');
+
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => {
+      
+      const firstTenPosts = data.slice(0, 10);
+      
+      firstTenPosts.forEach(post => {
+        const postDiv = document.createElement('div');
+        postDiv.classList.add('post');
+
+        postDiv.innerHTML = `
+          <h2>Title: ${post.title}</h2>
+          <p>Body: ${post.body}</p>
+        `;
+
+        postsContainer.appendChild(postDiv);
+      });
+    })
+    .catch(error => {
+      console.error('Error fetching posts:', error);
+      postsContainer.innerHTML = "<p>Failed to load posts.</p>";
+    });;
