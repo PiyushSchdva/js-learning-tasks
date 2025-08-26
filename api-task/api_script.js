cac71f57c1e63d0979a0c663879cd492e1b90b28
Piyush Sachdeva <PiyushSchdva>
2025-08-26 13:39:51 +0530

    Your commit message here

---
+++ api-task/api_script.js
@@ -0,0 +1,28 @@
+const postsContainer = document.getElementById('posts');
+
+async function loadPosts() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10');
+    const data = await response.json();
+
+    data.forEach(post => {
+      const postDiv = document.createElement('div');
+      postDiv.classList.add('post');
+
+      const title = document.createElement("h2");
+      title.textContent = `Title: ${post.title}`
+
+      const body = document.createElement("p")
+      body.textContent = `Body: ${post.body}`
+
+      postsContainer.appendChild(postDiv);
+      postDiv.append(title);
+      title.append(body);
+    });
+  } catch (error) {
+    console.error('Error fetching posts:', error);
+    postsContainer.innerHTML = "<p>Failed to load posts.</p>";
+  }
+}
+
+loadPosts();
