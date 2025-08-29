b957d01f250c291199287af3e68f778f65fa27eb
Piyush Sachdeva <PiyushSchdva>
2025-08-29 17:42:47 +0530

    api-3-commit

---
+++ api-task-3/script.js
@@ -0,0 +1,46 @@
+const postsContainer = document.getElementById('posts');
+const paginationContainer = document.getElementById('pagination');
+
+let allPosts = [];
+let currentPage = 1;
+const postsPerPage = 10;
+
+async function loadPosts() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    allPosts = await response.json();
+    renderPage(currentPage);
+    createPaginationControls();
+  } catch {
+    postsContainer.innerHTML = "<p>Failed to load posts.</p>";
+  }
+}
+
+function renderPage(page) {
+  postsContainer.innerHTML = '';
+  allPosts.slice((page-1)*postsPerPage, page*postsPerPage)
+          .forEach(post => {
+            postsContainer.innerHTML += `<div class="post">
+                                            <h2>Title: ${post.title}</h2>
+                                            <p>Body: ${post.body}</p>
+                                          </div>`;
+          });
+}
+
+function createPaginationControls() {
+  paginationContainer.innerHTML = '';
+  const totalPages =(allPosts.length / postsPerPage);
+  for (let i = 1; i <= totalPages; i++) {
+    const button = document.createElement('button');
+    button.innerText = i;
+    if(i === currentPage) button.style.backgroundColor = 'blue';
+    button.addEventListener('click', () => {
+      currentPage = i;
+      renderPage(currentPage);
+      createPaginationControls();
+    });
+    paginationContainer.appendChild(button);
+  }
+}
+
+loadPosts();
